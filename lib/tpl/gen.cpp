ec92a61a2681c6d78884b18cbae97ad92130d412
ClHg2 <ClHg2@outlook.com>
2024-07-13 22:49:19 +0800

    style(gen): do explicit casting in a more consistent way

--- lib/tpl/gen.cpp
+++ lib/tpl/gen.cpp
@@ -2,7 +2,7 @@
 
 #include <ext/random>
 
-__gnu_cxx::sfmt19937 rng{(unsigned)__rdtsc()};
+__gnu_cxx::sfmt19937 rng{unsigned(__rdtsc())};
 ttt > il T rand(T l, T r) {
   assert(l <= r);
   return uniform_int_distribution<T>{l, r}(rng);
